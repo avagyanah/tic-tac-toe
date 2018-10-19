962949c1f1445a360c173a037056a46d303e2d81
Ashot Avagyan <avagyanah@gmail.com>
2018-10-19 22:15:38 +0400

    atlas

--- src/view/scenes/UIScene.ts
+++ src/view/scenes/UIScene.ts
@@ -15,10 +15,6 @@ export class UIScene extends BaseScene {
     this.createUI();
     //
     super.create();
-    //
-    setTimeout(() => {
-      Register.emit(UI_SCENE_FAME_CLICK);
-    }, 1300);
   }
 
   public async sleep(): Promise<void> {
